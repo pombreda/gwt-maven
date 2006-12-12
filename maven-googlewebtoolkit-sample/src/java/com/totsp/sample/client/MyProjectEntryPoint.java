04cb765c808d91ca899584bd4d9739c012c21273
charlie.collins <charlie.collins@a4611b4d-791b-0410-a941-edd6d174b497>
2006-12-12 22:04:32 +0000

    jalopied

--- maven-googlewebtoolkit-sample/src/java/com/totsp/sample/client/MyProjectEntryPoint.java
+++ maven-googlewebtoolkit-sample/src/java/com/totsp/sample/client/MyProjectEntryPoint.java
@@ -1,7 +1,5 @@
 package com.totsp.sample.client;
 
-import java.util.List;
-
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.Window;
@@ -14,15 +12,18 @@ import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
+
 import com.totsp.sample.client.model.Entry;
+
+import java.util.List;
 
 
 /**
  * EntryPoint example of a GWT RPC call,
  * in order to demonstrate using the TotSP Maven GWT plugin:
  * http://code.google.com/p/gwt-maven.
- * 
- * In the real world do not put all your crap in an EntryPoint like this 
+ *
+ * In the real world do not put all your crap in an EntryPoint like this
  * (this is an example which is meant to be quick and dirty).
  *
  * @author ccollins
@@ -75,19 +76,20 @@ public class MyProjectEntryPoint implements EntryPoint {
         service.myMethod(s,
             new AsyncCallback() {
                 // what to do if it works
-                public void onSuccess(Object result) {                    
+                public void onSuccess(Object result) {
                     List entries = (List) result;
                     StringBuffer resultBuffer = new StringBuffer();
-                    if (entries != null)
-                    {
-                        for (int i=0; i < entries.size(); i++)
-                        {
+
+                    if (entries != null) {
+                        for (int i = 0; i < entries.size(); i++) {
                             Entry entry = (Entry) entries.get(i);
-                            resultBuffer.append("name - " + entry.name + "  | time - " + entry.time + "\n");
+                            resultBuffer.append("name - " + entry.name +
+                                "  | time - " + entry.time + "\n");
                         }
                     }
-                    
-                    l.setText("The current DataSource store result set is: \n" + resultBuffer.toString());
+
+                    l.setText("The current DataSource store result set is: \n" +
+                        resultBuffer.toString());
                 }
 
                 // what to do if it fails
--- maven-googlewebtoolkit-sample/src/java/com/totsp/sample/util/DBConnTest.java
+++ maven-googlewebtoolkit-sample/src/java/com/totsp/sample/util/DBConnTest.java
@@ -1,5 +1,7 @@
 package com.totsp.sample.util;
 
+import org.hsqldb.jdbcDriver;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -7,28 +9,25 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import org.hsqldb.jdbcDriver;
 
 /**
- * Stupid simple, naive, never do this type JDBC example, just to test HSQL. 
- * 
+ * Stupid simple, naive, never do this type JDBC example, just to test HSQL.
+ *
  * @author ccollins
  *
  */
-public class DBConnTest
-{
+public class DBConnTest {
     private static final String JDBC_URL = "jdbc:hsqldb:mem:testdb";
     private static final String JDBC_DRIVER = "org.hsqldb.jdbcDriver";
     private static final String JDBC_USER = "sa";
     private static final String JDBC_PASS = "";
 
-    public static void testConnection()
-    {
+    public static void testConnection() {
         Connection conn = null;
         Statement st = null;
         ResultSet rs = null;
-        try
-        {
+
+        try {
             Class.forName(JDBC_DRIVER);
             DriverManager.registerDriver(new jdbcDriver());
             conn = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASS);
@@ -36,14 +35,18 @@ public class DBConnTest
             st = conn.createStatement();
 
             // create a table to do something with
-            st.executeUpdate("CREATE TABLE sample ( id INTEGER IDENTITY, name VARCHAR(256), time VARCHAR(256))");
+            st.executeUpdate(
+                "CREATE TABLE sample ( id INTEGER IDENTITY, name VARCHAR(256), time VARCHAR(256))");
             conn.commit();
             System.out.println("created sample table");
 
             // put some stuff IN the table
-            st.executeUpdate("INSERT INTO sample (name, time) VALUES ('test1', 'party like its 1999')");
-            st.executeUpdate("INSERT INTO sample (name, time) VALUES ('test2', 'the time, what time is it')");
-            st.executeUpdate("INSERT INTO sample (name, time) VALUES ('test3', 'time flies')");
+            st.executeUpdate(
+                "INSERT INTO sample (name, time) VALUES ('test1', 'party like its 1999')");
+            st.executeUpdate(
+                "INSERT INTO sample (name, time) VALUES ('test2', 'the time, what time is it')");
+            st.executeUpdate(
+                "INSERT INTO sample (name, time) VALUES ('test3', 'time flies')");
             conn.commit();
             System.out.println("added some stuff to the sample table");
 
@@ -51,53 +54,43 @@ public class DBConnTest
             rs = st.executeQuery("SELECT * FROM sample");
             System.out.println("getting stuff from sample table \n");
             System.out.println(DBConnTest.display(rs));
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
-        }
-        finally
-        {
-            try
-            {
+        } finally {
+            try {
                 st.close();
                 conn.close();
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 // gulp
             }
         }
     }
 
-    public static String display(ResultSet rs)
-    {
+    public static String display(ResultSet rs) {
         StringBuffer sb = new StringBuffer();
-        try
-        {
+
+        try {
             ResultSetMetaData meta = rs.getMetaData();
             int colmax = meta.getColumnCount();
             int i;
             Object o = null;
-            while (rs.next())
-            {
-                for (i = 0; i < colmax; ++i)
-                {
+
+            while (rs.next()) {
+                for (i = 0; i < colmax; ++i) {
                     o = rs.getObject(i + 1);
                     sb.append(o.toString() + " \n");
                 }
+
                 sb.append(" \n");
             }
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
+
         return sb.toString();
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("call testConnection");
         DBConnTest.testConnection();
     }
