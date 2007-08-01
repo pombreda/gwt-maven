0ce253b319a93b10c42695b3da6ab8fe7b0748e0
charlie.collins <charlie.collins@a4611b4d-791b-0410-a941-edd6d174b497>
2007-08-01 17:09:41 +0000

    imports organized

--- maven-googlewebtoolkit-support/src/test/com/totsp/mavenplugin/gwt/support/MakeCatalinaBaseTest.java
+++ maven-googlewebtoolkit-support/src/test/com/totsp/mavenplugin/gwt/support/MakeCatalinaBaseTest.java
@@ -7,12 +7,7 @@
 
 package com.totsp.mavenplugin.gwt.support;
 
-import junit.framework.*;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import junit.framework.TestCase;
 
 /**
  *
