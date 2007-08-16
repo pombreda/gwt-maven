93608c60fe117211c24f65393a9bfa4c5eaf20f4
kebernet <kebernet@a4611b4d-791b-0410-a941-edd6d174b497>
2007-08-16 20:35:46 +0000

    1.5.5

---
+++ src/test/test/server/BBean.java
@@ -0,0 +1,69 @@
+/*
+ * ABean.java
+ *
+ * Created on November 25, 2006, 9:26 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package test.server;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+
+import java.util.List;
+import java.util.Arrays;
+
+/**
+ *
+ * @author cooper
+ */
+public class BBean {
+    
+    private Float floatProperty;
+    private boolean booleanProperty;
+    private List<String>[] strings;
+    
+    /** Creates a new instance of ABean */
+    public BBean() {
+    }
+
+    public Float getFloatProperty() {
+        return floatProperty;
+    }
+
+    public void setFloatProperty(Float floatProperty) {
+        this.floatProperty = floatProperty;
+    }
+
+    public boolean isBooleanProperty() {
+        return booleanProperty;
+    }
+
+    public void setBooleanProperty(boolean booleanProperty) {
+        this.booleanProperty = booleanProperty;
+    }
+
+    public List<String>[] getStrings() {
+        return strings;
+    }
+
+    public void setStrings(List<String>[] strings) {
+        this.strings = strings;
+    }
+    
+    public boolean equals(Object object) {
+    BBean t = (BBean) object;
+
+    EqualsBuilder eb = new EqualsBuilder();
+
+    eb.append(strings, t.strings)
+        .append(floatProperty, floatProperty)
+        .append(booleanProperty, t.booleanProperty);
+
+    return eb.isEquals();
+  }
+    
+    
+    
+}
