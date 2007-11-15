857ade3a8fef3518c495f30d9363a84c53b59b5e
ahhughes@gmail.com <ahhughes@gmail.com@a4611b4d-791b-0410-a941-edd6d174b497>
2007-11-15 02:09:35 +0000

    Initial import. Module/Code stub's only... need to confirm if this should include EJB's + External Shell (Glassfish/JBoss) eta. I have put them in for now. I think GWT without this isn't very useful.

---
+++ trunk/maven-googlewebtoolkit2-sample/rpc/src/main/java/com/totsp/mavenplugin/gwt/sample/client/ExampleRemoteServiceAsync.java
@@ -0,0 +1,7 @@
+package com.totsp.mavenplugin.gwt.sample.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public interface ExampleRemoteServiceAsync {
+	public void doComplimentMe(AsyncCallback callback);
+}
