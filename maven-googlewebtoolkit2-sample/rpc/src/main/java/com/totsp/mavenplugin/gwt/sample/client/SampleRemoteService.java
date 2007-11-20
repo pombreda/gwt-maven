c92c684199c413ebe86fe7007e8f3ad6848196ba
ahhughes@gmail.com <ahhughes@gmail.com@a4611b4d-791b-0410-a941-edd6d174b497>
2007-11-20 00:29:40 +0000

    Removed references to EJB & EAR modules. Removed any use of EJB's and replaced with a  Java5+ enum to demonstrate the separation of client (Java1.4 GWT) and server (Java5+) code within this multi-module maven project. Also renamed some classes from "Example" to "Sample" for consistent naming across the project. NOTE: THIS PROJECT STILL WILL NOT BUILD "OUT OF THE BOX", WE NEED TO ADDRESS THE GWT PROFILE STILL, either settings.xml or project specific???? suggestions please - Andrew.

--- maven-googlewebtoolkit2-sample/rpc/src/main/java/com/totsp/mavenplugin/gwt/sample/client/SampleRemoteService.java
+++ maven-googlewebtoolkit2-sample/rpc/src/main/java/com/totsp/mavenplugin/gwt/sample/client/SampleRemoteService.java
@@ -2,6 +2,6 @@ package com.totsp.mavenplugin.gwt.sample.client;
 
 import com.google.gwt.user.client.rpc.RemoteService;
 
-public interface ExampleRemoteService extends RemoteService {
+public interface SampleRemoteService extends RemoteService {
 	public String doComplimentMe();
 }
