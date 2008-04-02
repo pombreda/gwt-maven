55623469d748a1d290c3c721d505e7ef078f18cf
charlie.collins <charlie.collins@a4611b4d-791b-0410-a941-edd6d174b497>
2008-04-02 12:55:12 +0000

    accidentally added target output, cleaning that up

---
+++ trunk/maven-googlewebtoolkit2-sample/rpc/target/classes/com/totsp/mavenplugin/gwt/sample/client/SampleRemoteServiceAsync.java
@@ -0,0 +1,7 @@
+package com.totsp.mavenplugin.gwt.sample.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public interface SampleRemoteServiceAsync {
+	public void doComplimentMe(AsyncCallback callback);
+}
