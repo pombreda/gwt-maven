965e1c53945c9d0854e720a9aeb91d168b63d762
daniel@sendula.com <daniel@sendula.com@a4611b4d-791b-0410-a941-edd6d174b497>
2008-05-01 10:43:47 +0000

    Added different way of invoking GWTCompiler

--- maven-googlewebtoolkit2-plugin/src/main/java/com/totsp/mavenplugin/gwt/ScriptWriterWindows.java
+++ maven-googlewebtoolkit2-plugin/src/main/java/com/totsp/mavenplugin/gwt/ScriptWriterWindows.java
@@ -11,9 +11,13 @@ package com.totsp.mavenplugin.gwt;
 import org.apache.maven.artifact.DependencyResolutionRequiredException;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.net.URL;
 
 import java.util.Collection;
 
@@ -38,7 +42,7 @@ public class ScriptWriterWindows {
 
         for (File f : classpath) {
             cpString.append("\"" + f.getAbsolutePath() + "\";");
-            //break the line at 4000 characters to avout max size.
+            //break the line at 4000 characters to about max size.
             if (cpString.length() > 4000) {
                 writer.println(cpString);
                 cpString = new StringBuffer();
@@ -83,53 +87,92 @@ public class ScriptWriterWindows {
         return file;
     }
 
+    public File writeRunScriptNew(AbstractGWTMojo mojo) throws IOException, DependencyResolutionRequiredException {
+
+        writeClassPathFile(mojo);
+        writeClasspathInvoker(mojo);
+
+        String filename = (mojo instanceof DebugMojo) ? "debug.cmd" : "run.cmd";
+        File file = new File(mojo.getBuildDir(), filename);
+        PrintWriter writer = new PrintWriter(new FileWriter(file));
+
+        String targetDir = mojo.getBuildDir().getAbsolutePath();
+
+        String extra = (mojo.getExtraJvmArgs() != null) ? mojo.getExtraJvmArgs() : "";
+
+        writer.print("\"" + AbstractGWTMojo.JAVA_COMMAND + "\" " + extra + " -cp \"" + targetDir + "\" ");
+
+        if (mojo instanceof DebugMojo) {
+            writer.print(" -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,address=");
+            writer.print(mojo.getDebugPort());
+            writer.print(",suspend=y ");
+        }
+
+        writer.print("-Dcatalina.base=\"" + mojo.getTomcat().getAbsolutePath() + "\" ");
+        writer.print(" ClasspathInvoker \"" + targetDir + "/classpath\" ");
+        writer.print(" com.google.gwt.dev.GWTShell");
+        writer.print(" -gen \"");
+        writer.print(mojo.getGen().getAbsolutePath());
+        writer.print("\" -logLevel ");
+        writer.print(mojo.getLogLevel());
+        writer.print(" -style ");
+        writer.print(mojo.getStyle());
+        writer.print(" -out ");
+        writer.print("\"" + mojo.getOutput().getAbsolutePath() + "\"");
+        writer.print(" -port ");
+        writer.print(Integer.toString(mojo.getPort()));
+
+        if (mojo.isNoServer()) {
+            writer.print(" -noserver ");
+        }
+
+        writer.print(" " + mojo.getRunTarget());
+        writer.println();
+        writer.flush();
+        writer.close();
+
+        return file;
+    }
+
     public File writeCompileScript(AbstractGWTMojo mojo)
         throws IOException, DependencyResolutionRequiredException {
+
+        writeClasspathInvoker(mojo);
+        writeClassPathFile(mojo);
+
         File file = new File(mojo.getBuildDir(), "compile.cmd");
         PrintWriter writer = new PrintWriter(new FileWriter(file));
-        Collection<File> classpath = mojo.buildClasspathList(false);
-        writer.print("set CLASSPATH=");
 
-        StringBuffer cpString = new StringBuffer();
-
-        for (File f : classpath) {
-            cpString.append("\"" + f.getAbsolutePath() + "\";");
-
-            //break the line at 4000 characters to avout max size.
-            if (cpString.length() > 4000) {
-                writer.println(cpString);
-                cpString = new StringBuffer();
-                writer.print("set CLASSPATH=%CLASSPATH%;");
-            }
-        }
-        writer.println(cpString);
-        writer.println();
+        String targetDir = mojo.getBuildDir().getAbsolutePath();
 
         for (String target : mojo.getCompileTarget()) {
             String extra = (mojo.getExtraJvmArgs() != null) ? mojo.getExtraJvmArgs() : "";
-            writer.print("\"" + AbstractGWTMojo.JAVA_COMMAND + "\" " + extra + " -cp %CLASSPATH% ");
+
+            writer.print("\"" + AbstractGWTMojo.JAVA_COMMAND + "\" " + extra + " -cp \"" + targetDir + "\" ");
+            writer.print(" ClasspathInvoker \"" + targetDir + "/classpath\" ");
             writer.print(" com.google.gwt.dev.GWTCompiler ");
             writer.print(" -gen \"");
             writer.print(mojo.getGen().getAbsolutePath());
             writer.print("\" -logLevel ");
             writer.print(mojo.getLogLevel());
             writer.print(" -style ");
+            writer.print(" ");
             writer.print(mojo.getStyle());
 
             writer.print(" -out ");
-            writer.print(mojo.getOutput().getAbsolutePath());           
+            writer.print(mojo.getOutput().getAbsolutePath());
             writer.print(" ");
-            
+
             if (mojo.isEnableAssertions()) {
                 writer.print(" -ea ");
             }
 
-            writer.print(target);
+            writer.print(" " + target);
             writer.println();
 
             // if gwtVersion is NOT 1.3 or 1.4, then assume it is 1.5 or higher, and move "std" and "xs" linker directories to target
             if(!(mojo.getGwtVersion().startsWith("1.3.") || mojo.getGwtVersion().startsWith("1.4."))){
-                //TODO change this to inspect linker output
+
                 String std = mojo.getOutput().getAbsolutePath() + "\\" + target + "\\std";
                 writer.println("move " + std + "\\* " + mojo.getOutput().getAbsolutePath() + "\\" + target);
                 writer.println("del " + std + " /S /Q");
@@ -144,5 +187,49 @@ public class ScriptWriterWindows {
         writer.close();
 
         return file;
+    }
+
+    protected void writeClasspathInvoker(AbstractGWTMojo mojo) throws IOException {
+
+        File file = new File(mojo.getBuildDir(), "ClasspathInvoker.class");
+        URL url = getClass().getClassLoader().getResource("/ClasspathInvoker.class");
+        InputStream in = url.openStream();
+
+        copyResource(in, file);
+    }
+
+    protected void copyResource(InputStream in, File outFile) throws IOException {
+
+        OutputStream out = new FileOutputStream(outFile);
+        try {
+            try {
+                byte[] buffer = new byte[20480];
+                int r = in.read(buffer);
+                while (r > 0) {
+                    out.write(buffer, 0, r);
+                    r = in.read(buffer);
+                }
+            } finally {
+                in.close();
+            }
+        } finally {
+            out.close();
+        }
+    }
+
+    protected void writeClassPathFile(AbstractGWTMojo mojo)
+        throws IOException, DependencyResolutionRequiredException {
+
+        File file = new File(mojo.getBuildDir(), "classpath");
+        PrintWriter writer = new PrintWriter(new FileWriter(file));
+        try {
+            Collection<File> classpath = mojo.buildClasspathList(false);
+
+            for (File f : classpath) {
+                writer.println(f.getAbsolutePath());
+            }
+        } finally {
+            writer.close();
+        }
     }
 }
