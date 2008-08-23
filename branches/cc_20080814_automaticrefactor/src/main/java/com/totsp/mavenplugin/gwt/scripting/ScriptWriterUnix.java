819f5e6107ae612cb08d4f033d685652fc8c775f
charlie.collins@gmail.com <charlie.collins@gmail.com@a4611b4d-791b-0410-a941-edd6d174b497>
2008-08-23 02:25:17 +0000

    update license

--- branches/cc_20080814_automaticrefactor/src/main/java/com/totsp/mavenplugin/gwt/scripting/ScriptWriterUnix.java
+++ branches/cc_20080814_automaticrefactor/src/main/java/com/totsp/mavenplugin/gwt/scripting/ScriptWriterUnix.java
@@ -1,10 +1,18 @@
 /*
- * ScriptWriterUnix.java
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * Created on November 23, 2007, 12:18 PM
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
  */
 package com.totsp.mavenplugin.gwt.scripting;
 
