d437215f27b2c85e50efe200c1f9291333da367f
charlie.collins@gmail.com <charlie.collins@gmail.com@a4611b4d-791b-0410-a941-edd6d174b497>
2008-09-14 12:35:40 +0000

    bug fixes for file errors when using clean and various webXml and contextXml scenarios (webXml only required if mergewebxml goal is enabled, otherwise default empty is used for shell - contextXml never required, but can be specified and used with shell, but will NOT always be same as regular source context.xml because of different Tomcat versions between shell and deploy time)

--- trunk/maven-googlewebtoolkit2-plugin/src/main/java/com/totsp/mavenplugin/gwt/GWTMojo.java
+++ trunk/maven-googlewebtoolkit2-plugin/src/main/java/com/totsp/mavenplugin/gwt/GWTMojo.java
@@ -78,11 +78,20 @@ public class GWTMojo extends AbstractGWTMojo {
      * @throws Exception
      */
     public void makeCatalinaBase() throws Exception {
-        getLog().debug("make catalina base for embedded Tomcat");
+        getLog().debug("make catalina base for embedded Tomcat");        
+        
+        if (this.getWebXml() != null && this.getWebXml().exists()) {
+            this.getLog().info("source web.xml present - " + this.getWebXml() + " - using it with embedded Tomcat");
+        } else {
+            this.getLog().info("source web.xml NOT present, using default empty web.xml for shell");
+        }
+
+        // note that MakeCatalinaBase (support jar) will use emptyWeb.xml if webXml does not exist 
         String[] args = { this.getTomcat().getAbsolutePath(), this.getWebXml().getAbsolutePath() };
         MakeCatalinaBase.main(args);
 
         if ((this.getContextXml() != null) && this.getContextXml().exists()) {
+            this.getLog().info("contextXml parameter present - " + this.getContextXml() + " - using it for embedded Tomcat ROOT.xml");
             FileUtils.copyFile(this.getContextXml(), new File(this.getTomcat(), "conf/gwt/localhost/ROOT.xml"));
         }
     }
