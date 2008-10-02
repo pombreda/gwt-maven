269d0b406b3aadb562b49be6a0d90b43f9c48b27
charlie.collins <charlie.collins@a4611b4d-791b-0410-a941-edd6d174b497>
2008-10-02 17:27:04 +0000

    leopard error output patch

--- trunk/maven-googlewebtoolkit2-plugin/src/main/java/com/totsp/mavenplugin/gwt/scripting/ScriptUtil.java
+++ trunk/maven-googlewebtoolkit2-plugin/src/main/java/com/totsp/mavenplugin/gwt/scripting/ScriptUtil.java
@@ -46,14 +46,35 @@ public final class ScriptUtil {
       }
 
       try {
-         pw.startProcess(out, err);         
-         pw.waitFor();         
+         pw.startProcess(out, err);
+         pw.waitFor();
 
-         // if err has anything it's an exception
+         // if err has anything it's an exception - excepting special Leopard stuff
          if (err.length() > 0) {
-            throw new MojoExecutionException("error attempting to run test - " + exec.getName() + " - " + err.toString());
-         } 
-         
+            boolean validError = true;
+
+            // the Mac VM will log CocoaComponent messages to stderr, falsely triggering the exception
+            if (AbstractGWTMojo.OS_NAME.startsWith(AbstractGWTMojo.MAC)) {
+               validError = false;
+               final String[] errLines = err.toString().split("\n");
+               for (int i = 0; i < errLines.length; ++i) {
+                  final String currentErrLine = errLines[i].trim();
+                  if (!currentErrLine.endsWith("[JavaCocoaComponent compatibility mode]: Enabled")
+                           && !currentErrLine
+                                    .endsWith("[JavaCocoaComponent compatibility mode]: Setting timeout for SWT to 0.100000")
+                           && currentErrLine.length() != 0) {
+                     validError = true;
+                     break;
+                  }
+               }
+            }
+
+            if (validError) {
+               throw new MojoExecutionException("error attempting to run test - " + exec.getName() + " - "
+                        + err.toString());
+            }
+         }
+
          // otherwise populate and return the TestResult
          //
          // SUCCESS ends up in system.out "OK"
@@ -66,27 +87,29 @@ public final class ScriptUtil {
          // Tests run: 1,  Failures: 0,  Errors: 1
          // 
          // OK (1 test)
-         
+
          String[] lines = null;
          if (AbstractGWTMojo.OS_NAME.startsWith(AbstractGWTMojo.WINDOWS)) {
-        	 lines = out.toString().split("\r\n");
-         } else {
-        	 lines = out.toString().split("\n");
-         }         
-         String lastLine = lines[lines.length - 1];         
+            lines = out.toString().split("\r\n");
+         }
+         else {
+            lines = out.toString().split("\n");
+         }
+         String lastLine = lines[lines.length - 1];
          testResult.lastLine = lastLine;
          if (lastLine.indexOf("Tests run") != -1) {
             // TODO add parsing to differentiate FAILURE and ERROR, or BOTH?
             testResult.code = TestCode.FAILURE;
-         } else if (lastLine.indexOf("OK") != -1) {
+         }
+         else if (lastLine.indexOf("OK") != -1) {
             testResult.code = TestCode.SUCCESS;
          }
-         testResult.message = out.toString();               
+         testResult.message = out.toString();
       }
       catch (Exception e) {
-         throw new MojoExecutionException("error attempting to run test - " + exec.getName() + " - " + e.getMessage(), e);
-      }      
+         throw new MojoExecutionException("error attempting to run test - " + exec.getName() + " - " + e.getMessage(),
+                  e);
+      }
       return testResult;
-   }   
+   }
 }
-
