4bdc764cef2938db3ba6359587013692d9bd1232
marek.romanowski <marek.romanowski@a4611b4d-791b-0410-a941-edd6d174b497>
2008-12-31 18:17:13 +0000

    Moved to m-gwt-support

--- maven-googlewebtoolkit2-plugin/src/main/java/com/totsp/mavenplugin/gwt/util/DependencyScope.java
+++
@@ -1,6 +0,0 @@
-package com.totsp.mavenplugin.gwt.util;
-
-// this was an inner class on BuildClasspathUtil - but Maven chokes on that with a Qdox parse exception?
-public enum DependencyScope {
-    COMPILE, RUNTIME, TEST
-}
